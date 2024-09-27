6fa385dd4f0445a6febc6c23054cf6e4dea0c760
Aleksei Kostiukov <a.kostiukov2011@yandex.ru>
2024-09-28 00:27:06 +0300

    Revert "новое расположеие index"
    This reverts commit e387a3c5cc043e6bd2ac49bacf3ee12fcfee4282.

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   devtool: 'source-map',
   plugins: [
     new HtmlPlugin({
-      template: './index.html',
+      template: 'docs/index.html',
     }),
     new CopyPlugin({
       patterns: [
